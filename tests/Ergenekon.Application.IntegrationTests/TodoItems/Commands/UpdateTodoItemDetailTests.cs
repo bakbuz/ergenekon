291a04bfec52a5144e8492dd74d35fe452d9bdb2
Bayram Akbuz <bayram.akbuz@gmail.com>
2023-06-22 09:51:51 +0300

    Yeniden Ergenekon

---
+++ tests/Ergenekon.Application.IntegrationTests/TodoItems/Commands/UpdateTodoItemDetailTests.cs
@@ -0,0 +1,61 @@
+﻿using Ergenekon.Application.Common.Exceptions;
+using Ergenekon.Application.TodoItems.Commands.CreateTodoItem;
+using Ergenekon.Application.TodoItems.Commands.UpdateTodoItem;
+using Ergenekon.Application.TodoItems.Commands.UpdateTodoItemDetail;
+using Ergenekon.Application.TodoLists.Commands.CreateTodoList;
+using Ergenekon.Domain.Entities;
+using Ergenekon.Domain.Enums;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Ergenekon.Application.IntegrationTests.TodoItems.Commands;
+
+using static Testing;
+
+public class UpdateTodoItemDetailTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireValidTodoItemId()
+    {
+        var command = new UpdateTodoItemCommand { Id = 99, Title = "New Title" };
+        await FluentActions.Invoking(() => SendAsync(command)).Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Test]
+    public async Task ShouldUpdateTodoItem()
+    {
+        var userId = await RunAsDefaultUserAsync();
+
+        var listId = await SendAsync(new CreateTodoListCommand
+        {
+            Title = "New List"
+        });
+
+        var itemId = await SendAsync(new CreateTodoItemCommand
+        {
+            ListId = listId,
+            Title = "New Item"
+        });
+
+        var command = new UpdateTodoItemDetailCommand
+        {
+            Id = itemId,
+            ListId = listId,
+            Note = "This is the note.",
+            Priority = PriorityLevel.High
+        };
+
+        await SendAsync(command);
+
+        var item = await FindAsync<TodoItem>(itemId);
+
+        item.Should().NotBeNull();
+        item!.ListId.Should().Be(command.ListId);
+        item.Note.Should().Be(command.Note);
+        item.Priority.Should().Be(command.Priority);
+        item.LastModifiedBy.Should().NotBeNull();
+        item.LastModifiedBy.Should().Be(userId);
+        item.LastModifiedAt.Should().NotBeNull();
+        item.LastModifiedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+    }
+}
---
+++ tests/Ergenekon.Application.IntegrationTests/TodoItems/Commands/UpdateTodoItemTests.cs
@@ -0,0 +1,55 @@
+﻿using Ergenekon.Application.Common.Exceptions;
+using Ergenekon.Application.TodoItems.Commands.CreateTodoItem;
+using Ergenekon.Application.TodoItems.Commands.UpdateTodoItem;
+using Ergenekon.Application.TodoLists.Commands.CreateTodoList;
+using Ergenekon.Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Ergenekon.Application.IntegrationTests.TodoItems.Commands;
+
+using static Testing;
+
+public class UpdateTodoItemTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireValidTodoItemId()
+    {
+        var command = new UpdateTodoItemCommand { Id = 99, Title = "New Title" };
+        await FluentActions.Invoking(() => SendAsync(command)).Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Test]
+    public async Task ShouldUpdateTodoItem()
+    {
+        var userId = await RunAsDefaultUserAsync();
+
+        var listId = await SendAsync(new CreateTodoListCommand
+        {
+            Title = "New List"
+        });
+
+        var itemId = await SendAsync(new CreateTodoItemCommand
+        {
+            ListId = listId,
+            Title = "New Item"
+        });
+
+        var command = new UpdateTodoItemCommand
+        {
+            Id = itemId,
+            Title = "Updated Item Title"
+        };
+
+        await SendAsync(command);
+
+        var item = await FindAsync<TodoItem>(itemId);
+
+        item.Should().NotBeNull();
+        item!.Title.Should().Be(command.Title);
+        item.LastModifiedBy.Should().NotBeNull();
+        item.LastModifiedBy.Should().Be(userId);
+        item.LastModifiedAt.Should().NotBeNull();
+        item.LastModifiedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+    }
+}
