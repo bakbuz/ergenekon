bc768bb9200ab0f4e0ffb28a2a8df890dfe69d05
Bayram Akbuz <bayram.akbuz@gmail.com>
2023-06-26 08:19:48 +0300

    Yeniden Ergenekon

---
+++ src/Ergenekon.Host/Services/WebHelper.cs
@@ -0,0 +1,16 @@
+﻿using Ergenekon.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ergenekon.Host.Services;
+
+public class WebHelper : IWebHelper
+{
+    public string GetCurrentApplicationUrl()
+    {
+        return "";
+    }
+}
