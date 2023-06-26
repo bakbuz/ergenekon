aeb12507b33a98ae00f5eeed61fbe003d4e5c016
Bayram Akbuz <bayram.akbuz@gmail.com>
2023-06-26 18:45:00 +0300

    Yeniden Ergenekon

--- src/Ergenekon.Host/Services/WebHelper.cs
+++ src/Ergenekon.Host/Services/WebHelper.cs
@@ -1,5 +1,7 @@
 ﻿using Ergenekon.Application.Common.Interfaces;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
 using System.Text;
 using System.Text.Encodings.Web;
 
@@ -7,9 +9,27 @@ namespace Ergenekon.Host.Services;
 
 public class WebHelper : IWebHelper
 {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public WebHelper(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
     public string GetCurrentApplicationUrl()
     {
-        return "";
+        if (_httpContextAccessor.HttpContext == null)
+            return string.Empty;
+
+        bool useSSL = _httpContextAccessor.HttpContext.Request.Scheme == "https";
+
+        //try to get host from the request HOST header
+        var hostHeader = _httpContextAccessor.HttpContext.Request.Headers[HeaderNames.Host];
+        if (StringValues.IsNullOrEmpty(hostHeader))
+            return string.Empty;
+
+        //add scheme to the URL
+        return $"{(useSSL ? Uri.UriSchemeHttps : Uri.UriSchemeHttp)}{Uri.SchemeDelimiter}{hostHeader.FirstOrDefault()}";
     }
 
     public string GetEmailConfirmCallbackUrl(string userId, string code)
