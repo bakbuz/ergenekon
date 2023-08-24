36d13104f71e2377d50441a3b5ff110ce4b7f953
Bayram Akbuz <bayram.akbuz@gmail.com>
2023-08-24 08:52:15 +0300

    Yeniden Ergenekon

--- src/Ergenekon.Host/Program.cs
+++ src/Ergenekon.Host/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, loggerConfig) => loggerConfig
     .WriteTo.Console()
+    //.WriteTo.Seq("http://localhost:5341")
     .ReadFrom.Configuration(context.Configuration));
 
 // Add services to the container.
