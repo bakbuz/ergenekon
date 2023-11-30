71376c138c6b379f34631fd0138c9ba75663ae14
Bayram Akbuz <bayram.akbuz@gmail.com>
2023-11-30 15:57:58 +0300

    UserController

---
+++ backend/Ergenekon.Host/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ergenekon.Host.Controllers;
+
+public class AuthController : ApiControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
--- backend/Ergenekon.Host/Controllers/UserController.cs
+++ backend/Ergenekon.Host/Controllers/UserController.cs
@@ -3,15 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Ergenekon.Host.Controllers;
 
-public class AuthController : ApiControllerBase
-{
-    [HttpGet]
-    public IActionResult Get()
-    {
-        return Ok();
-    }
-}
-
 [Authorize]
 public class UserController : ApiControllerBase
 {
